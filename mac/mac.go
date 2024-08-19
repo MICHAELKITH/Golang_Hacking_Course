05f1715a2d8fd126b8483489dd496c7411ecf13c
MICHAELKITH <michaelkithinji27@gmail.com>
2024-08-19 23:32:20 +0300

    changing mac

---
+++ mac/mac.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+    "flag"
+    "fmt"
+    "os"
+    "os/exec"
+)
+
+func main() {
+    interfaceName := flag.String("interface", "", "Name of the network interface")
+    newMacAddress := flag.String("mac", "", "New MAC address")
+    flag.Parse()
+
+    if *interfaceName == "" || *newMacAddress == "" {
+        fmt.Println("Usage: change_mac -interface [interface_name] -mac [new_mac_address]")
+        os.Exit(1)
+    }
+
+    // Disable the network interface
+    exec.Command("sudo", "ifconfig", *interfaceName, "down").Run()
+
+    // Change the MAC address
+    exec.Command("sudo", "ifconfig", *interfaceName, "hw", "ether", *newMacAddress).Run()
+
+    // Re-enable the network interface
+    exec.Command("sudo", "ifconfig", *interfaceName, "up").Run()
+
+    fmt.Println("MAC address changed successfully")
+}
