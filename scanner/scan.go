d32c85f9b3f91b93992d7f37ed3259486a8a054e
MICHAELKITH <michaelkithinji27@gmail.com>
2024-08-20 22:32:11 +0300

    How to scan

---
+++ scanner/scan.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+    "fmt"
+    "github.com/google/gopacket/pcap"
+)
+
+func main() {
+    devices, err := pcap.FindAllDevs()
+    if err != nil {
+        fmt.Println("Error finding devices:", err)
+        return
+    }
+
+    for _, device := range devices {
+        fmt.Println("Device Name:", device.Name)
+        fmt.Println("Device Description:", device.Description)
+    }
+}
