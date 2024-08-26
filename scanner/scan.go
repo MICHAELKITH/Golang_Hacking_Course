4c662b27581a76b4a767a4a4410c5cfb8deef043
MICHAELKITH <michaelkithinji27@gmail.com>
2024-08-26 23:13:34 +0300

    Hello world in go

--- scanner/scan.go
+++ scanner/scan.go
@@ -1,19 +1,6 @@
 package main
 
-import (
-    "fmt"
-    "github.com/google/gopacket/pcap"
-)
-
-func main() {
-    devices, err := pcap.FindAllDevs()
-    if err != nil {
-        fmt.Println("Error finding devices:", err)
-        return
-    }
-
-    for _, device := range devices {
-        fmt.Println("Device one:", device.Name)
-        fmt.Println("Device Description:", device.Description)
-    }
+  import "fmt"
+func main(){
+    fmt.Println("Hello Gophers ")
 }
