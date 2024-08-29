a6ceedda8a93f9c5c155e8808676c180bb93a970
MICHAELKITH <michaelkithinji27@gmail.com>
2024-08-29 23:02:23 +0300

    Playing around with channels

--- scanner/scan.go
+++ scanner/scan.go
@@ -2,5 +2,19 @@ package main
 
   import "fmt"
 func main(){
-    fmt.Println("Hello Gophers ")
+    c := make(chan int)
+
+    go strl("Michael", c)
+
+
+    x := <- c
+
+    fmt.Println(" The string length is ", x)
+}
+
+
+func strl(s string , c chan int){
+
+  c <- len(s)
+
 }
